eff707ff9747346aa10574032df9b32c98b09a88
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-11-23 19:06:35 -0300

    create Calculator .jsx&.css

---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,9 @@
+import './Calculator.css';
+
+const Calculator = () => {
+   return (
+      <div className='col-10 col-lg-6 my-3 mx-auto rounded container-fluid row calculator'></div>
+   );
+};
+
+export default Calculator;
