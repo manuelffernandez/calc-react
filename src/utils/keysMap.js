42b4bd622ca9caaaf91fef80064347ca7de0707b
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-11-26 14:32:16 -0300

    refact keysMap object

--- src/utils/keysMap.js
+++ src/utils/keysMap.js
@@ -1,76 +1,120 @@
-const keysMap = {
-   numbers: [
-      {
-         id: "7",
-         no: 7
-      },
-      {
-         id: "8",
-         no: 8
-      },
-      {
-         id: "9",
-         no: 9
-      },
-      {
-         id: "4",
-         no: 4
-      },
-      {
-         id: "5",
-         no: 5
-      },
-      {
-         id: "6",
-         no: 6
-      },
-      {
-         id: "1",
-         no: 1
-      },
-      {
-         id: "2",
-         no: 2
-      },
-      {
-         id: "3",
-         no: 3
-      },
-      {
-         id: "0",
-         no: 0
-      },
-      {
-         id: "10",
-         no: "."
-      },
-   ],
-   operators: [
-      {
-         id: "11",
-         op: "C"
-      },
-      {
-         id: "12",
-         op: "/"
-      },
-      {
-         id: "13",
-         op: "*"
-      },
-      {
-         id: "14",
-         op: "-"
-      },
-      {
-         id: "15",
-         op: "+"
-      },
-      {
-         id: "16",
-         op: "="
-      },
-   ]
-}
+const typesMap = {
+  number: 'number',
+  comma: 'comma',
+  operator: 'operator',
+  equal: 'equal',
+  clear: 'clear',
+};
 
-export default keysMap
+const keypadLayoutMap = {
+  right: 'right',
+  left: 'left',
+};
+
+const keysMap = [
+  {
+    id: '7',
+    char: '7',
+    layoutSide: keypadLayoutMap.left,
+    type: typesMap.number,
+  },
+  {
+    id: '8',
+    char: '8',
+    layoutSide: keypadLayoutMap.left,
+    type: typesMap.number,
+  },
+  {
+    id: '9',
+    char: '9',
+    layoutSide: keypadLayoutMap.left,
+    type: typesMap.number,
+  },
+  {
+    id: '4',
+    char: '4',
+    layoutSide: keypadLayoutMap.left,
+    type: typesMap.number,
+  },
+  {
+    id: '5',
+    char: '5',
+    layoutSide: keypadLayoutMap.left,
+    type: typesMap.number,
+  },
+  {
+    id: '6',
+    char: '6',
+    layoutSide: keypadLayoutMap.left,
+    type: typesMap.number,
+  },
+  {
+    id: '1',
+    char: '1',
+    layoutSide: keypadLayoutMap.left,
+    type: typesMap.number,
+  },
+  {
+    id: '2',
+    char: '2',
+    layoutSide: keypadLayoutMap.left,
+    type: typesMap.number,
+  },
+  {
+    id: '3',
+    char: '3',
+    layoutSide: keypadLayoutMap.left,
+    type: typesMap.number,
+  },
+  {
+    id: '0',
+    char: '0',
+    layoutSide: keypadLayoutMap.left,
+    type: typesMap.number,
+  },
+  {
+    id: '10',
+    char: '.',
+    layoutSide: keypadLayoutMap.left,
+    type: typesMap.comma,
+  },
+
+  {
+    id: '11',
+    char: 'C',
+    layoutSide: keypadLayoutMap.right,
+    type: typesMap.clear,
+  },
+  {
+    id: '12',
+    char: '/',
+    layoutSide: keypadLayoutMap.right,
+    type: typesMap.operator,
+  },
+  {
+    id: '13',
+    char: '*',
+    layoutSide: keypadLayoutMap.right,
+    type: typesMap.operator,
+  },
+  {
+    id: '14',
+    char: '-',
+    layoutSide: keypadLayoutMap.right,
+    type: typesMap.operator,
+  },
+  {
+    id: '15',
+    char: '+',
+    layoutSide: keypadLayoutMap.right,
+    type: typesMap.operator,
+  },
+  {
+    id: '16',
+    char: '=',
+    layoutSide: keypadLayoutMap.right,
+    type: typesMap.equal,
+  },
+];
+
+export default keysMap;
